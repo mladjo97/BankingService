d3d8569f19c8b7426e16b7f19a100a75b364bf19
unknown <>
2018-11-06 15:26:51 +0100

    Added BankingSector project

---
+++ BankingService/BankingSectors/AccountServices.cs
@@ -0,0 +1,17 @@
+﻿using CommonStuff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSectors
+{
+    public class AccountServices : IAccountServices
+    {
+        public bool OpenAccount(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
